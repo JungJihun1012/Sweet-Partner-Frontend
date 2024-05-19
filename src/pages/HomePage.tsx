f20c1f3e8062e4e586a735f909ffbfaef8a4aaa7
Tech5818 <pqow5818@gmail.com>
2024-05-19 21:53:23 +0900

    :sparkles: kakao map 컴포넌트 호출

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { Mobile } from "../Responsive";
 import { UserPartnerInfo } from "../components/UserPartnerInfo/UserPartnerInfo";
+import KakaoMap from "../components/Kakao/KakaoMap";
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <Container>
         <Mobile>
           <UserPartnerInfo />
+          <KakaoMap level={4} myLoca={{ lat: 33.5563, lng: 126.79581 }} />
         </Mobile>
       </Container>
     </>
